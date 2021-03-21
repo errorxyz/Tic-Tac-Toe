d332ca327e124828bdf5a2a3699ffa535605feaf
errorxyz <64748057+errorxyz@users.noreply.github.com>
2021-03-21 12:39:59 +0530

    Add files via upload

---
+++ README.txt
@@ -0,0 +1,21 @@
+The online multiplayer game involves a server and a client, however
+while playing there is no actual difference between them.
+There is some setup required to play the game between the
+server and client.
+
+In order to play within the same(wifi) network we need to send any one
+file to the person we wish to play with and then modify the 
+host variable in both the files to the local IP Address of the device 
+running the 'server' file and then run the files in the respective
+devices. In order to find the local IP Address of the device running
+the 'server' file, open Command Prompt in windows and enter 'ipconfig'.
+A line in the output will be similar to the line below:
+IPv4 Address . . . . . . . . . . . : 192.168.x.x
+Copy this number, modify the host variable and you are good to go!
+
+
+In order to play from any part of the world, the process is quite 
+complex, yet it is possible to play by port forwarding using ngrok or
+by configuring your router.
+
+Thank You!
---
+++ ttt-client.py
@@ -0,0 +1,206 @@
+import pygame
+import socket
+
+pygame.init()#initialising
+pygame.display.set_caption('Tac-Tic-Toe')
+
+#colors
+white=(255,255,255)
+black=(0,0,0)
+red=(255,0,0)
+green=(0,155,0)
+
+#sets window size and creates window
+disp = 600
+gameDisplay = pygame.display.set_mode((disp, disp))
+
+#fonts
+smallfont = pygame.font.SysFont("comicsansms", 20)
+medfont = pygame.font.SysFont("comicsansms",35)
+largefont = pygame.font.SysFont("comicsansms", 50)
+
+#images
+ximg = pygame.image.load('x200.png')
+oimg = pygame.image.load('o200.png')
+
+#possible coordinates of x and o
+possible_pos = [(0,0),(disp/3,0),(disp*2/3,0),(0,disp/3),(disp/3,disp/3),(disp*2/3,disp/3),(0,disp*2/3),(disp/3,disp*2/3),(disp*2/3,disp*2/3)]
+winList = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[1,5,9],[3,5,7]]#possible win combinarions
+usedXPos = []#contains positions of x
+usedOPos = []#contains positions of o
+turn = -1
+
+clock = pygame.time.Clock()#used for setting fps
+
+#establishing connection
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+host = "127.0.0.1"#IP Address of server
+port = 1234#OPTIONAL PORT
+s.connect((host, port))#connects to server
+
+#refer server file for more detailed documentation
+
+def text_objects(text,color, size):
+	if size == "small":
+		textSurface = smallfont.render(text, True,color)
+	elif size == "medium":
+		textSurface = medfont.render(text, True,color)
+	elif size == "large":
+		textSurface = largefont.render(text, True,color)
+	return textSurface, textSurface.get_rect()
+
+def msg_to_screen(msg, color, y_displace = 0, size="small"):
+	textSurf, textRect = text_objects(msg,color, size)
+	textRect.center = (disp/2), (disp /2)+y_displace
+	gameDisplay.blit(textSurf, textRect)
+
+def draw(pos):#drawing x or o
+	global turn
+	global usedXPos
+	global usedOPos
+
+	if not pos in usedXPos and not pos in usedOPos: #if input is not already used then
+                turn += 1
+                if turn % 2 == 0:
+                        dX(pos)
+			
+                if turn % 2 == 1:
+                        dO(pos)
+
+def Check(list):#checks if draw or win or nothing
+	global winList
+	win = False
+	draw = False
+	
+	for i in range(8):
+		if all(x in list for x in winList[i]):#if used list is a superlist of any one element of win list then
+			win = True
+			msg_to_screen("press q to quit", black, 50, "large")
+			clock.tick(5)
+			
+		if len(usedXPos+usedOPos) == 9:#if the length of used lists is 9 then its a draw
+			draw = True
+			msg_to_screen("press q to quit", black, 50,"large")
+			clock.tick(5)
+	#these if statements are to handle the situation where a player wins and the all the boxes are filled
+	if win == True:
+		msg_to_screen("player "+str(turn%2+1)+" has won", red, size="large")
+		
+	elif draw == True and win == False:
+		msg_to_screen("Its a Draw", red, size="large")		
+
+def dO(pos):#draws o
+	
+	gameDisplay.blit(oimg, (possible_pos[pos - 1][0], possible_pos[pos-1][1]))
+	
+	usedOPos.append(pos)
+	Check(usedOPos)
+	
+	pygame.display.update()#updates screen
+
+def dX(pos):#draws x
+	
+	gameDisplay.blit(ximg, (possible_pos[pos - 1][0], possible_pos[pos-1][1]))
+	
+	usedXPos.append(pos)
+	Check(usedXPos)
+	
+	pygame.display.update()#updates screen
+
+def Grid():#draws grid with the numbers
+	
+	pygame.draw.line(gameDisplay, black, (disp/3,0),(disp/3,1000))
+	pygame.draw.line(gameDisplay, black,(disp*(2/3),0),(disp*(2/3),1000))
+	pygame.draw.line(gameDisplay, black, (0,disp/3),(1000, disp/3))
+	pygame.draw.line(gameDisplay, black, (0, disp*(2/3)), (1000,disp*(2/3)))
+	for i in range(9):
+		text = medfont.render(str(i+1),True, black)
+		gameDisplay.blit(text,(possible_pos[i][0],possible_pos[i][1]))
+
+def gameIntro():
+	
+	intro = True
+	while intro:
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+				
+			if event.type == pygame.KEYDOWN:
+				if event.key == pygame.K_q:#if input is q
+					pygame.quit()
+					quit()
+				elif event.key == pygame.K_p:#if input is p
+					intro = False
+					
+		gameDisplay.fill(white)
+		msg_to_screen("Welcome to Tic-Tac-Toe", green, y_displace=-50,size="large")
+		msg_to_screen("Press p to play q to quit", black,y_displace = 0, size="medium")
+		msg_to_screen("Enter the number corresponding to", black, y_displace = 50, size="medium")
+		msg_to_screen("the box to add x or o", black, y_displace = 100, size="medium")
+		msg_to_screen("1  2  3", black,y_displace=150,size="small")
+		msg_to_screen("4  5  6", black,y_displace=200,size="small")
+		msg_to_screen("7  8  9", black,y_displace=250,size="small")
+		
+		pygame.display.update()
+		clock.tick(5)#Determines frames per second
+		
+def gameLoop():
+	global pos
+	
+	gameDisplay.fill(white)
+	Grid()
+
+	pygame.display.update()
+	
+	while True:
+		
+		for event in pygame.event.get():
+			if event.type == pygame.QUIT:
+				pygame.quit()
+				quit()
+				s.close()
+			if turn % 2 == 0:
+				if event.type == pygame.KEYDOWN:
+					if event.key == pygame.K_q:
+						pygame.quit()
+						quit()
+						s.close()
+					elif event.key == pygame.K_1:#if input is 1
+						draw(1)
+						s.send(bytes("1",'utf-8'))
+					elif event.key == pygame.K_2:#if input is 2
+						draw(2)
+						s.send(bytes("2",'utf-8'))
+					elif event.key == pygame.K_3:
+						draw(3)
+						s.send(bytes("3",'utf-8'))
+					elif event.key == pygame.K_4:
+						draw(4)
+						s.send(bytes("4",'utf-8'))
+					elif event.key == pygame.K_5:
+						draw(5)
+						s.send(bytes("5",'utf-8'))
+					elif event.key == pygame.K_6:
+						draw(6)
+						s.send(bytes("6",'utf-8'))
+					elif event.key == pygame.K_7:
+						draw(7)
+						s.send(bytes("7",'utf-8'))
+					elif event.key == pygame.K_8:
+						draw(8)
+						s.send(bytes("8",'utf-8'))
+					elif event.key == pygame.K_9:
+						draw(9)
+						s.send(bytes("9",'utf-8'))
+						
+			elif turn % 2 == 1:
+				position = int(str(s.recv(1024)).replace("b","").replace("'",""))
+				draw(position)
+	
+		pygame.display.update()
+		clock.tick(3)
+		
+gameIntro()
+gameLoop()
